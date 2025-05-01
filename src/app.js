8e0fb7739d76d37ee6974be28091dca076856211
DevKeria <keriawiley@yahoo.com>
2025-05-01 22:18:15 +0000

    domain name

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { ServerHMRConnector } from "vite";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const pronouns = ['my', 'your', 'our'];
+  const adjectives = ['awesome', 'cool', 'amazing'];
+  const nouns = ['website', 'project', 'business'];
+  
+  const domainNames = [];
+  
+  for (const pronoun of pronouns) {
+      for (const adjective of adjectives) {
+          for (const noun of nouns) {
+              const domainName = pronoun + adjective + noun + '.com';
+              domainNames.push(domainName);
+          }
+      }
+  }
+  
+  domainNames.forEach(domainName => {
+      console.log(domainName);
+  }'
