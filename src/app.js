1c08ffc2a8a6a0b1be4909a75125be0b475e0443
DevKeria <keriawiley@yahoo.com>
2025-05-09 23:20:09 +0000

    site is functional now

--- src/app.js
+++ src/app.js
@@ -1,28 +1,32 @@
+
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import { ServerHMRConnector } from "vite";
+
 
 window.onload = function() {
-  //write your code here
-  const pronouns = ['my', 'your', 'our'];
-  const adjectives = ['awesome', 'cool', 'amazing'];
-  const nouns = ['website', 'project', 'business'];
-  
-  const domainNames = [];
-  
-  for (const pronoun of pronouns) {
-      for (const adjective of adjectives) {
-          for (const noun of nouns) {
-              const domainName = pronoun + adjective + noun + '.com';
-              domainNames.push(domainName);
-          }
-      }
-  }
-  
-  domainNames.forEach(domainName => {
-      console.log(domainName);
-  }'
+     const pronouns = ['my', 'your', 'our'];
+     const adjectives = ['awesome', 'cool', 'amazing'];
+     const nouns = ['website', 'project', 'business'];
+
+     const domainNames = [];
+
+     for (const pronoun of pronouns) {
+       for (const adjective of adjectives) {
+         for (const noun of nouns) {
+           const domainName = pronoun + adjective + noun + '.com';
+           domainNames.push(domainName);
+         }
+       }
+     }
+
+     const outputElement = document.createElement('div');
+     domainNames.forEach(domainName => {
+       const p = document.createElement('p');
+       p.textContent = domainName;
+       outputElement.appendChild(p);
+     });
+
+     document.body.appendChild(outputElement);
+   };
