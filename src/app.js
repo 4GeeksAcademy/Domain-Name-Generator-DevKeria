84646c1a7eeffe37fd3df78adf971ec0203cc7fd
DevKeria <keriawiley@yahoo.com>
2025-05-25 00:35:52 +0000

    workiung

--- src/app.js
+++ src/app.js
@@ -8,18 +8,11 @@ window.onload = function () {
   const adjectives = ['awesome', 'cool', 'amazing'];
   const nouns = ['website', 'project', 'business'];
 
-  
   const randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
   const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
 
   const domainName = randomPronoun + randomAdjective + randomNoun + '.com';
 
- 
-  const outputElement = document.createElement('div');
-  const p = document.createElement('p');
-  p.textContent = domainName;
-  outputElement.appendChild(p);
-
-  document.body.appendChild(outputElement);
+  console.log(domainName);
 };
